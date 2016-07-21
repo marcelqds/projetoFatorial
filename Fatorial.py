0c58318620069988bdcd65e8184500924089d972
Marcelo Queiroz de Santana <marcelqds@hotmail.com>
2016-07-21 15:32:06 -0300

    Criação da primeira versão da classe Fatorial.py

---
+++ Fatorial.py
@@ -0,0 +1,21 @@
+class Fatorial:
+        def __init__(self, valor):
+                self.valor = int(valor)
+                self.resultadoFatorial()         
+
+        def resultadoFatorial(self):
+                self.resultado = 1
+                for i in range(2,self.valor+1):
+                        self.resultado *= i
+                        print "--- %d = %d" %(i,self.resultado)
+
+        def __str__(self) :
+                return "Fatorial de %d = %d" %(self.valor, self.resultado)
+        
+
+
+fat = Fatorial(4)
+print fat
+	
+	
+
