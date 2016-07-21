454a470e6a25bd5b21030189f0c2c92559daeade
Marcelo Queiroz de Santana <marcelqds@hotmail.com>
2016-07-21 18:16:34 -0300

    Criação do arquivo Principal testeFatorial.py para instanciar Fatorial.

---
+++ testeFatorial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from Fatorial import Fatorial
+print "----- Program from calculete Fatorial ----- \n\n"
+numero = int(raw_input("Insert number Fatorial: "))
+
+fat = Fatorial(numero)
+print fat
+
+
+
+
+
+
+
